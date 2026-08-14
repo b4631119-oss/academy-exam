5c59379b620a9de04c69e3d505979761daa0256d
DevRoot <b4631119@gmail.com>
2026-08-14 23:54:34 +0600

    hello

--- src/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { proxy, config } from './proxy'
-import type { NextRequest } from 'next/server'
-
-export async function middleware(request: NextRequest) {
-  return proxy(request)
-}
-
-export { config }
