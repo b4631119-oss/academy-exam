664ef393f461f789a53cbdc99e32d5194624bf2a
DevRoot <b4631119@gmail.com>
2026-09-02 21:18:56 +0600

    feat: integrate ToastProvider into root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/lib/theme-provider";
+import { ToastProvider } from "@/components/ToastProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -121,6 +122,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       </head>
       <body className="min-h-full flex flex-col">
         <ThemeProvider>{children}</ThemeProvider>
+        <ToastProvider />
       </body>
     </html>
   );
